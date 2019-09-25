2827cca3b7e732f146aa268a1d63b605ed5b1ab3
pkhlopiachyi <khlpavlo@gmail.com>
2019-09-25 17:15:57 +0000

    [WIP] create acc in progress

--- src/js/focus.js
+++ src/js/focus.js
@@ -2,15 +2,31 @@ window.onload = function() {
     let email = document.getElementById('email');
     let passwd = document.getElementById('passwd');
     let forgotEmail = document.getElementById('forgot-email');
-    
+
     let errMes = document.getElementById('err-message');
     let errBlock = document.getElementById('error-block');
-    
+
     let loginSendButton = document.getElementById('login-send');
     let forgotSendButton = document.getElementById('forgot-send-btn');
-    
+
     let validEmail = false;
     let validPasswd = false;
+
+    let emailSignUp = document.getElementById('email-signup');
+    let passwdSignUp = document.getElementById('passwd-signup');
+    console.log(passwdSignUp);
+    let passwdSignUpConfirm = document.getElementById('passwd-signup-confirm');
+
+    let errMesPasswd = document.getElementById('err-message');
+    let errMesPasswdBlock = document.getElementById('error-signup-block');
+    let errMesPasswdConfirm =document.getElementById('err-message-confirm');
+    let errMesPasswdConfirmBlock = document.getElementById('error-signup-confirm-block');
+
+    let createButton = document.getElementById("signup-send");
+
+    let validEmailSignUp = false;
+    let validPasswdSignUp = false;
+    let validPasswdSignupConfirm = false;
 
     window.onFocusInput = (borderBlock) => {
         document.getElementById(borderBlock).style.border = "1px solid #E84A5F";
@@ -19,27 +35,23 @@ window.onload = function() {
     window.onBlurInput = (borderBlock) => {
         document.getElementById(borderBlock).style.border = "1px solid rgba(238, 238, 238, 0.2)";
     }
-    
+
     email.onkeyup = () => {
-    
+
         validEmail = testEmail(email);
         enableLoginButton(validPasswd, validEmail);
-    }    
-    
+    }
+
     forgotEmail.onkeyup = () => {
         let res = testEmail(forgotEmail);
         console.log(res);
         if (res) {
-            forgotSendButton.disabled = false;
-            forgotSendButton.style.background = "#E84A5F";
-            forgotSendButton.style.cursor = "pointer";
+            enableButton(forgotSendButton);
         } else {
-            forgotSendButton.disabled = true;
-            forgotSendButton.style.background = "rgba(232, 74, 95, 0.5)";
-            forgotSendButton.style.cursor = "not-allowed";
+            disableButton(forgotSendButton);
         }
     }
-    
+
     function testEmail(email) {
         let str = email.value;
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -49,58 +61,90 @@ window.onload = function() {
             return false;
         }
     }
-    
-    window.onkeyup = () => {
+
+    passwd.onkeyup = () => {
         errBlock.style.display = "none";
-        validPasswd = testPasswd();
+        validPasswd = testPasswd(passwd, errMes, errBlock);
         enableLoginButton(validPasswd, validEmail);
     }
-    
-    function testPasswd() {
-        let str = passwd.value;
-    
-    if (
-        str.match(/[a-z]/g)) {
+
+    function testPasswd(input, errorMessage, errorBlock) {
+        let str = input.value;
+        console.log(input);
+        console.log(errorMessage);
+        console.log(errorBlock);
+        if (
+            str.match(/[a-z]/g)) {
             if (str.match(/[A-Z]/g)) {
                 if (str.match(/[0-9]/g)) {
                    if (str.match(/[^a-zA-Z\d]/g)) {
                         if (str.length >= 8) {
                             return true;
                         } else {
-                            animation(errMes, errBlock,"Password length shuld be at least 8 symbols");
+                            animation(errorMessage, errorBlock,"Password length shuld be at least 8 symbols");
                             return false;
                         }
                     } else {
-                        animation(errMes, errBlock, "Need at least 1 special character");
+                        animation(errorMessage, errorBlock, "Need at least 1 special character");
                         return false;
                     }
                 } else {
-                    animation(errMes, errBlock, "Need at least 1 number");
+                    animation(errorMessage, errorBlock, "Need at least 1 number");
                     return false;
                 }
             } else {
-    
-                animation(errMes, errBlock, "Need at least 1 uppercase character");
+
+                animation(errorMessage, errorBlock, "Need at least 1 uppercase character");
                 return false;
             }
         } else {
-            animation(errMes, errBlock, "Need at least 1 lowercase character");
+            animation(errorMessage, errorBlock, "Need at least 1 lowercase character");
             return false;
         }
     }
-    
-    
-    function animation(elem, block, message) {
-            elem.innerHTML = message;
-            fadeIn(block);
-            setTimeout(() => {
-                fadeOut(block);
-            }, 5000);
+
+    function signUpValid() {
+        if(validEmailSignUp && validPasswdSignUp && validPasswdSignUpConfirm) {
+            enableButton(createButton);
+        } else {
+            disableButton(createButton);
+        }
     }
-    
+
+    emailSignUp.onkeyup = () => {
+        validEmailSignUp = testEmail(emailSignUp);
+        signUpValid();
+    }
+
+    passwdSignUp.onkeyup = () => {
+        validPasswdSignUp = testPasswd(passwdSignUp, errMesPasswd, errMesPasswdBlock);
+        signUpValid();
+    }
+
+    passwdSignUpConfirm.onkeyup = () => {
+        let pass = paswdSignUp.value;
+        let passConfirm = passwdSignUpConfirm.value;
+
+        if(pass === passConfirm) {
+            validPasswdSignUpConfirm = true;
+        } else {
+            animation(errMesPasswdConfirm, errMesPasswdConfirmBlock, "Passwords don't equal")
+        }
+
+        signUpValid();
+    }
+
+    function animation(elem, block, message) {
+        elem.innerHTML = message;
+        fadeIn(block);
+        setTimeout(() => {
+            fadeOut(block);
+        }, 5000);
+    }
+
     function fadeOut(el){
       el.style.opacity = 1;
-    
+
       (function fade() {
         if ((el.style.opacity -= .1) < 0) {
           el.style.display = "none";
@@ -109,11 +153,11 @@ window.onload = function() {
         }
       })();
     }
-    
+
     function fadeIn(el){
       el.style.opacity = 0;
       el.style.display = "flex";
-    
+
       (function fade() {
         var val = parseFloat(el.style.opacity);
         if (!((val += .1) > 1)) {
@@ -122,19 +166,26 @@ window.onload = function() {
         }
       })();
     }
-    
+
     function enableLoginButton(valPasswd, valEmail) {
-    
+
         if(valEmail && valPasswd) {
-            loginSendButton.disabled = false;
-            loginSendButton.style.backgroundColor = "#E84A5F";
-            loginSendButton.style.cursor = "pointer";
+            enableButton(loginSendButton);
         } else {
-            loginSendButton.disabled = true;
-            loginSendButton.style.backgroundColor = "rgba(232, 74, 95, 0.5)";
-            loginSendButton.style.cursor = "not-allowed";
+            disableButton(loginSendButton);
         }
     }
-    
+
+    function enableButton(button) {
+        button.disabled = false;
+        button.style.backgroundColor = "#E84A5F";
+        button.style.cursor = "pointer";
+    }
+
+    function disableButton(button) {
+        button.disabled = true;
+        button.style.backgroundColor = "rgba(232, 74, 95, 0.5)";
+        button.style.cursor = "not-allowed";
+    }
 }
-    
+
